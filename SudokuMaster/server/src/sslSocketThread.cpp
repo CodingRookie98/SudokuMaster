48bafed920b5dcab189da5280f88cfbb680365d4
CodingRookie <520whjoker@gmail.com>
2023-07-01 00:56:29 +0800

    feat: Each connection works in the thread pool

---
+++ SudokuMaster/server/src/sslSocketThread.cpp
@@ -0,0 +1,34 @@
+﻿/**
+  ******************************************************************************
+  * @file           : sslSocketThread.cpp
+  * @author         : hui
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2023/6/30
+  ******************************************************************************
+  */
+
+#include "sslSocketThread.h"
+
+SslSocketThread::SslSocketThread(const QSharedPointer<QSslSocket>& sslSocket) : QRunnable(), mSslSocket(sslSocket) {
+    setAutoDelete(false);
+}
+
+SslSocketThread::~SslSocketThread() {
+    mSslSocket.reset();
+}
+
+void SslSocketThread::run() {
+    int();
+    signalProcess();
+}
+
+void SslSocketThread::init() {
+
+}
+
+void SslSocketThread::signalProcess() {
+//    connect(mSslSocket.get(), &QSslSocket::stateChanged, this, [](const QAbstractSocket::SocketState& state){
+//        qDebug() << "@@" << qt_error_string(state);
+//    });
+}
---
+++ SudokuMaster/server/src/sslSocketThread.h
@@ -0,0 +1,32 @@
+﻿/**
+  ******************************************************************************
+  * @file           : sslSocketThread.h
+  * @author         : hui
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2023/6/30
+  ******************************************************************************
+  */
+
+#ifndef SUDOKUMASTER_SSLSOCKETTHREAD_H
+#define SUDOKUMASTER_SSLSOCKETTHREAD_H
+
+#include <QSslSocket>
+#include <QRunnable>
+
+class SslSocketThread : public QObject, public QRunnable {
+    Q_OBJECT
+public:
+    explicit SslSocketThread(const QSharedPointer<QSslSocket>& sslSocket);
+    ~SslSocketThread() override;
+protected:
+    void run() override;
+private:
+    QSharedPointer<QSslSocket> mSslSocket;
+
+    void init();
+    void signalProcess();
+};
+
+
+#endif //SUDOKUMASTER_SSLSOCKETTHREAD_H
