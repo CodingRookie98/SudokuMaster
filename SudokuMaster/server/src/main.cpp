56c254fdac7c22ab0e22dc052f3f54df96bf5da2
CodingRookie <520whjoker@gmail.com>
2023-07-01 01:00:41 +0800

    fix: The server listening thread must be placed in the main thread

--- SudokuMaster/server/src/main.cpp
+++ SudokuMaster/server/src/main.cpp
@@ -3,15 +3,31 @@
 //
 
 #include <QCoreApplication>
-#include "tcpServer.h"
+#include "sslServer.h"
 #include "QDebug"
+#include <QThreadPool>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    qDeug() << "start";
-    tcpServer server(&a);
+    auto *server = new sslServer;
+    while (server == nullptr) {
+        server = new sslServer(&a);
+    }
+
+    QThreadPool::globalInstance()->start(server);
+    server->log(ServerLog::Info, "start work");
+
+    // 必须把监听端口放在主线程中执行，否则会
+    // QObject: Cannot create children for a parent that is in a different thread.
+    if (!server->listen(QHostAddress::Any, qint16(45678))) {
+        server->log(ServerLog::MessageType::Error, QString("Failed to create server"));
+        delete server;
+        a.exit();
+    }else if (server->isListening()) {
+        server->log(ServerLog::MessageType::Info,
+                       QString("Listening on port " + QString::number(server->serverPort())));
+    }
 
     return a.exec();
 }
