084e5751ee1fe72e2097301d752db40538fecec1
CodingRookie <520whjoker@gmail.com>
2023-07-02 00:42:53 +0800

    refactor: Add exception handling

--- SudokuMaster/server/src/main.cpp
+++ SudokuMaster/server/src/main.cpp
@@ -6,28 +6,31 @@
 #include "sslServer.h"
 #include "QDebug"
 #include <QThreadPool>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    auto *server = new sslServer;
-    while (server == nullptr) {
-        server = new sslServer(&a);
+    sslServer *server = nullptr;
+    try {
+        server = new sslServer;
+        QThreadPool::globalInstance()->start(server);
+        server->log(ServerLog::Info, "start work");
+
+        // 必须把监听端口放在主线程中执行，否则会
+        // QObject: Cannot create children for a parent that is in a different thread.
+        if (!server->listen(QHostAddress::Any, qint16(45678))) {
+            server->log(ServerLog::MessageType::Error, QString("Failed to create server"));
+            delete server;
+            QCoreApplication::exit();
+        } else if (server->isListening()) {
+            server->log(ServerLog::MessageType::Info,
+                        QString("Listening on port " + QString::number(server->serverPort())));
+        }
+    } catch (const std::bad_alloc &e) {
+        std::cout << "Failed to allocate memory: " << e.what() << std::endl;
+        QCoreApplication::exit();
     }
 
-    QThreadPool::globalInstance()->start(server);
-    server->log(ServerLog::Info, "start work");
-
-    // 必须把监听端口放在主线程中执行，否则会
-    // QObject: Cannot create children for a parent that is in a different thread.
-    if (!server->listen(QHostAddress::Any, qint16(45678))) {
-        server->log(ServerLog::MessageType::Error, QString("Failed to create server"));
-        delete server;
-        a.exit();
-    }else if (server->isListening()) {
-        server->log(ServerLog::MessageType::Info,
-                       QString("Listening on port " + QString::number(server->serverPort())));
-    }
-
-    return a.exec();
+    return QCoreApplication::exec();
 }
