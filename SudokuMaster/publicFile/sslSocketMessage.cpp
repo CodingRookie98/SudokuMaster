2dfe8531f142b7c7336daa7b05b5b5135ed0da6c
CodingRookie <520whjoker@gmail.com>
2023-07-02 23:58:30 +0800

    add: Used to define various messages sent and received in an established connection

---
+++ SudokuMaster/publicFile/sslSocketMessage.cpp
@@ -0,0 +1,38 @@
+﻿/**
+  ******************************************************************************
+  * @file           : sslSocketMessage.cpp
+  * @author         : hui
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2023/7/2
+  ******************************************************************************
+  */
+
+#include "sslSocketMessage.h"
+
+void sslSocketMessage::init() {
+
+    for (auto itr = MessageOwnerQStr.constBegin(); itr != MessageOwnerQStr.constEnd(); ++itr) {
+        auto key = itr.key();
+        const auto& val = itr.value();
+        QStrMessageOwner.insert(val, key);
+    }
+
+    for (auto itr = ClientMessageTypeQStr.constBegin(); itr != ClientMessageTypeQStr.constEnd(); ++itr) {
+        auto key = itr.key();
+        const auto& val = itr.value();
+        QStrClientMessageType.insert(val, key);
+    }
+
+    for (auto itr = ServerMessageTypeQStr.constBegin(); itr != ServerMessageTypeQStr.constEnd(); ++itr) {
+        auto key = itr.key();
+        const auto& val = itr.value();
+        QStrServerMessageType.insert(val, key);
+    }
+    
+    for (auto itr = UserInfoTypeQStr.constBegin(); itr != UserInfoTypeQStr.constEnd(); ++itr) {
+        auto key = itr.key();
+        const auto& val = itr.value();
+        QStrUserInfoType.insert(val, key);
+    }
+}
---
+++ SudokuMaster/publicFile/sslSocketMessage.h
@@ -0,0 +1,75 @@
+﻿/**
+  ******************************************************************************
+  * @file           : sslSocketMessage.h
+  * @author         : hui
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2023/7/2
+  ******************************************************************************
+  */
+
+#ifndef SUDOKUMASTER_SSLSOCKETMESSAGE_H
+#define SUDOKUMASTER_SSLSOCKETMESSAGE_H
+
+#include <QMap>
+#include <QString>
+
+
+namespace sslSocketMessage {
+    QString keyMessageOwner("MessageOwner");
+
+    enum MessageOwner {
+        LoginWnd,
+    };
+
+    static QMap<MessageOwner, QString> MessageOwnerQStr{
+            {MessageOwner::LoginWnd, "LoginWnd"},
+    };
+
+    static QMap<QString, MessageOwner> QStrMessageOwner;
+
+    enum UserInfoType {
+        Username,
+        Password,
+    };
+
+    static QMap<UserInfoType, QString> UserInfoTypeQStr {
+            {UserInfoType::Username, "Username"},
+            {Password, "Password"},
+    };
+
+    static QMap<QString, UserInfoType> QStrUserInfoType;
+
+    enum ClientMessageType {
+        SignUp,
+        LoginIn
+    };
+
+    static QMap<ClientMessageType, QString> ClientMessageTypeQStr{
+            {ClientMessageType::SignUp,  "SignUp"},
+            {ClientMessageType::LoginIn, "LoginIn"}
+    };
+
+    static QMap<QString, ClientMessageType> QStrClientMessageType;
+
+    enum ServerMessageType {
+        UsernameAlreadyExists,
+        UserNotExist,
+        WrongPassword,
+        SignUpSuccess,
+    };
+
+    static QMap<ServerMessageType, QString> ServerMessageTypeQStr{
+            {ServerMessageType::UsernameAlreadyExists, "UsernameAlreadyExists"},
+            {ServerMessageType::UserNotExist,          "UserNotExist"},
+            {ServerMessageType::WrongPassword,         "WrongPassword"},
+            {ServerMessageType::SignUpSuccess,         "SignUpSuccess"},
+    };
+
+    static QMap<QString, ServerMessageType> QStrServerMessageType;
+
+
+    void init();
+}
+
+#endif //SUDOKUMASTER_SSLSOCKETMESSAGE_H
