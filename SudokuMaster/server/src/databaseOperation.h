dc72573331ea40eb16517fd6336ad1c11f17ed11
CodingRookie <520whjoker@gmail.com>
2023-07-02 23:54:14 +0800

    add: Used to define database operation statements

---
+++ SudokuMaster/server/src/databaseOperation.h
@@ -0,0 +1,27 @@
+﻿/**
+  ******************************************************************************
+  * @file           : databaseOperation.h
+  * @author         : hui
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2023/7/2
+  ******************************************************************************
+  */
+
+#ifndef SUDOKUMASTER_DATABASEOPERATION_H
+#define SUDOKUMASTER_DATABASEOPERATION_H
+#include <QString>
+#include <QMap>
+
+enum DatabaseOperationType {
+    AddUser,
+    SelectUserName,
+    IsCorrectPassword,
+};
+
+static QMap<DatabaseOperationType, QString> DatabaseOperationQStr {
+        {DatabaseOperationType::AddUser, "SELECT addUser('%1', '%2')"}, // %1: userName, %2: password
+        {DatabaseOperationType::SelectUserName, "SELECT selectUserName('%1')"}, // %1: userName
+        {DatabaseOperationType::IsCorrectPassword, "SELECT isCorrectPassword('%1', '%2')"}, // %1: userName, %2: password
+};
+#endif //SUDOKUMASTER_DATABASEOPERATION_H
